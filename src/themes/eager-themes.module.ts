5f2cfb354b81a11e99b6b9aed3060d9db5be9745
Ben Stokmans <benstokmans@hotmail.nl>
2025-10-07 14:12:30 +0200

    feat: add themes

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as SURFEagerThemeModule } from './surf/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
@@ -12,7 +13,8 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
+    // DSpaceEagerThemeModule,
+    SURFEagerThemeModule,
     // CustomEagerThemeModule,
   ],
 })
